e65a80d97917d62d5707e32b29570617eac85437
Alexander Nunes <82026691+alexxnunes@users.noreply.github.com>
2025-08-31 21:53:22 -0300

    Add files via upload
    #7DaysOfCode

---
+++ Desafio 2 - Variaveis.js
@@ -0,0 +1,11 @@
+// - Qual o seu nome?
+// - Quantos anos você tem?
+// - Qual linguagem de programação você está estudando?
+// "Olá [nome], você tem [idade] anos e já está aprendendo [linguagem]!"
+
+let nome = prompt("Qual seu nome?");
+let idade = prompt("Qual sua idade?");
+let linguagem = prompt("Qual linguagem estuda?");
+
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}`);
+
